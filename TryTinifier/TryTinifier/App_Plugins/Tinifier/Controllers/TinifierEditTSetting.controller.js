5d69606873331933720c82ba3786b45fb48c6f07
SSA\aleksandr.chuikov <aleksandr.chuikov@ssa.group>
2018-05-16 11:27:15 +0300

    modified project, added Tinifier 1.4.3

---
+++ TryTinifier/TryTinifier/App_Plugins/Tinifier/Controllers/TinifierEditTSetting.controller.js
@@ -0,0 +1,40 @@
+﻿angular.module("umbraco").controller("Tinifier.TinifierEditTSetting.Controller", function ($scope, $http, $routeParams, notificationsService) {
+
+    // Get settings
+    $scope.timage = {};
+
+    // Fill select dropdown
+    $scope.options = [
+        { value: false, label: "False" },
+        { value: true, label: "True" }
+    ];
+
+    // Fill form from web api
+    $http.get("/umbraco/backoffice/api/TinifierSettings/GetTSetting").success(function (response) {
+        $scope.timage = response;
+    });
+
+    // Submit form with settings
+    $scope.submitForm = function() {
+        timage = $scope.timage;
+        $http.post("/umbraco/backoffice/api/TinifierSettings/PostTSetting", JSON.stringify(timage))
+            .success(function(response) {
+                notificationsService.success("Success", response.message);
+            }).error(function(response) {
+                if (response.Error === 1) {
+                    notificationsService.warning("Warning", response.message);
+                }
+                else {
+                    notificationsService.error("Error", response.message);
+                }
+            });
+    };
+
+    $scope.stopTinifing = function () {
+        $http.delete("/umbraco/backoffice/api/TinifierState/DeleteActiveState").success(function (response) {
+            notificationsService.success("Success", "Tinifing successfully stoped!");
+        }).error(function (response) {
+            notificationsService.error("Error", "Tinifing can`t stop now!");
+        });
+    };
+});
---
+++ TryTinifier/TryTinifier/App_Plugins/Tinifier/Controllers/TinyTImage.controller.js
@@ -0,0 +1,68 @@
+﻿angular.module("umbraco").controller("Tinifier.TinyTImage.Controller", function ($scope, $routeParams, $http, notificationsService, dialogService) {
+
+    // Get the ID from the route parameters (URL)
+    var timageId = $routeParams.id;
+
+    var arrOfNames = [];
+    var selectedImages = document.querySelectorAll(".-selected");
+
+    for (var i = 0; i < selectedImages.length; i++) {
+        var innerHtml = selectedImages[i].innerHTML;
+        var regex = /<img.*?src=['"](.*?)['"]/;
+        var src = regex.exec(innerHtml)[1];
+        var slice = src.split("?")[0];
+        arrOfNames.push(slice);
+    }
+
+    // RecycleBinFolderId
+    var recycleBinFolderId = "-21";
+
+    // Get from the API
+    $scope.timage = null;
+
+    // Tinify Image and show notification
+    $scope.tinify = function () {
+
+        // Check if user choose Image or recycle bin folder
+        if (timageId === recycleBinFolderId) {
+            notificationsService.error("Error", "You can`t tinify RecycleBin Folder!");
+            return;
+        }
+
+        if (timageId == null) {
+            notificationsService.error("Error", "If you wish to optimize full Media folder, please, go to Tinifier section and click Tinify everything");
+            return;
+        }
+
+        notificationsService
+            .add({
+                headline: "Tinifing started",
+                message: "click here for more details",
+                url: '/umbraco/#/tinifier',
+                type: 'success'
+            });
+
+        var url = arrOfNames.length !== 0
+            ? "/umbraco/backoffice/api/Tinifier/TinyTImage?" + $.param({ imageRelativeUrls: arrOfNames, mediaId: 0 })
+            : "/umbraco/backoffice/api/Tinifier/TinyTImage?mediaId=" + timageId;
+
+        $http.get(url)
+            .success(successHandler)
+            .error(errorHandler);
+
+    };
+
+    $scope.tinifyAll = function () {
+        $http.put("/umbraco/backoffice/api/Tinifier/TinifyEverything")
+            .success(successHandler)
+            .error(errorHandler);
+    };
+
+    function successHandler(response) {
+        notificationsService.add(response);
+    }
+
+    function errorHandler(response) {
+        notificationsService.add(response);
+    }
+});
