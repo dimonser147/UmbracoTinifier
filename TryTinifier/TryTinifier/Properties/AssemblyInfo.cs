ce1a6ba3af91ec216027cf11279f895343a36cc8
SSA\aleksandr.chuikov <aleksandr.chuikov@ssa.group>
2018-05-16 18:06:41 +0300

    Modified added content for "Sandbox"

--- TryTinifier/TryTinifier/Properties/AssemblyInfo.cs
+++ TryTinifier/TryTinifier/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("799280dc-8de4-4162-aeb9-b53ff0ba5653")]
+[assembly: Guid("1e4e7164-1e1f-4e66-8ab2-2427276884c4")]
 
 // Version information for an assembly consists of the following four values:
 //
