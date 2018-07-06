beb5ff08a4c87e386d970c7c88a4ab53e2cff504
SSA\aleksandr.chuikov <aleksandr.chuikov@ssa.group>
2018-07-06 16:48:45 +0300

    Sorting in folder modified

--- Tinifier.Core/Services/Media/Organizers/ImageOrganizer.cs
+++ Tinifier.Core/Services/Media/Organizers/ImageOrganizer.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Tinifier.Core.Infrastructure;
 using Tinifier.Core.Models.API;
 using umbraco;
+using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using uMedia = Umbraco.Core.Models.Media;
 
@@ -43,69 +44,104 @@ namespace Tinifier.Core.Services.Media.Organizers
 
         private void ProcessPreviewModel()
         {
-            foreach (var media in _previewModel)
+            #region Old
+            //foreach (var media in _previewModel)
+            //{
+            //    int parentFolderId = _sourceFolderId;
+            //    string year = media.DestinationPath[0];
+            //    string month = media.DestinationPath[1];
+
+            //    foreach (var folderName in media.DestinationPath)
+            //    {
+            //        if (folderName == year)
+            //        {
+            //            //check if year folder exists
+            //            var yearFolder = uQuery.GetMediaByName(folderName).FirstOrDefault();
+            //            if (yearFolder == null)
+            //            {
+            //                var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
+            //                parentFolderId = folderMedia.Id;
+            //            }
+            //            else
+            //            {
+            //                parentFolderId = yearFolder.Id;
+            //            }
+            //        }
+
+            //        if (folderName == month)
+            //        {
+            //            //check if month folder exists
+            //            var monthFolder = uQuery.GetMediaByName(folderName).FirstOrDefault();
+            //            var yearFolder = uQuery.GetMediaByName(year).FirstOrDefault();
+            //            if (yearFolder != null)
+            //            {
+            //                if (monthFolder == null)
+            //                {
+            //                    var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, yearFolder.Id, PackageConstants.FolderAlias);
+            //                    parentFolderId = folderMedia.Id;
+            //                }
+            //                else
+            //                {
+            //                    if (monthFolder.ParentId == yearFolder.Id)
+            //                    {
+            //                        parentFolderId = monthFolder.Id;
+            //                    }
+            //                    else
+            //                    {
+            //                        var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
+            //                        parentFolderId = folderMedia.Id;
+            //                    }
+            //                }
+            //            }
+            //            else
+            //            {
+            //                if (monthFolder == null)
+            //                {
+            //                    var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
+            //                    parentFolderId = folderMedia.Id;
+            //                }
+            //                else
+            //                {
+            //                    parentFolderId = monthFolder.Id;
+            //                }
+            //            }
+            //        }
+            //    }
+            //    _imageService.Move(media.Media, parentFolderId);
+            //}
+
+            #endregion
+           
+            //int yearFolderId = 0;
+            //int monthFolderId = 0;
+            foreach (var item in _previewModel)
             {
                 int parentFolderId = _sourceFolderId;
-                string year = media.DestinationPath[0];
-                string month = media.DestinationPath[1];
-
-                foreach (var folderName in media.DestinationPath)
+                string year = item.DestinationPath[0];
+                string month = item.DestinationPath[1];
+                IMedia folderYear;
+                IMedia folderMonth;
+                var checkIfFolderYearExists = uQuery.GetMediaByName(year).FirstOrDefault(s => s.Parent.Id == parentFolderId);
+                if (checkIfFolderYearExists == null)
                 {
-                    if (folderName == year)
-                    {
-                        //check if year folder exists
-                        var yearFolder = uQuery.GetMediaByName(folderName).FirstOrDefault();
-                        if (yearFolder == null)
-                        {
-                            var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
-                            parentFolderId = folderMedia.Id;
-                        }
-                        else
-                        {
-                            parentFolderId = yearFolder.Id;
-                        }
-                    }
-
-                    if (folderName == month)
-                    {
-                        //check if month folder exists
-                        var monthFolder = uQuery.GetMediaByName(folderName).FirstOrDefault();
-                        var yearFolder = uQuery.GetMediaByName(year).FirstOrDefault();
-                        if (yearFolder != null)
-                        {
-                            if (monthFolder == null)
-                            {
-                                var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, yearFolder.Id, PackageConstants.FolderAlias);
-                                parentFolderId = folderMedia.Id;
-                            }
-                            else
-                            {
-                                if (monthFolder.ParentId == yearFolder.Id)
-                                {
-                                    parentFolderId = monthFolder.Id;
-                                }
-                                else
-                                {
-                                    var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
-                                    parentFolderId = folderMedia.Id;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (monthFolder == null)
-                            {
-                                var folderMedia = _mediaService.CreateMediaWithIdentity(folderName, parentFolderId, PackageConstants.FolderAlias);
-                                parentFolderId = folderMedia.Id;
-                            }
-                            else
-                            {
-                                parentFolderId = monthFolder.Id;
-                            }
-                        }
-                    }
+                    folderYear = _mediaService.CreateMediaWithIdentity(year, parentFolderId, PackageConstants.FolderAlias);
+                    parentFolderId = folderYear.Id;
                 }
-                _imageService.Move(media.Media, parentFolderId);
+                else
+                {
+                    parentFolderId = checkIfFolderYearExists.Id;
+                }
+                var checkIfFolderMonthExists = uQuery.GetMediaByName(month).FirstOrDefault(s => s.Parent.Id == parentFolderId);
+                if (checkIfFolderMonthExists == null)
+                {
+                    folderMonth = _mediaService.CreateMediaWithIdentity(month, parentFolderId, PackageConstants.FolderAlias);
+                    parentFolderId = folderMonth.Id;
+                }
+                else
+                {
+                    parentFolderId = checkIfFolderMonthExists.Id;
+                }
+                _imageService.Move(item.Media, parentFolderId);
             }
         }
     }
